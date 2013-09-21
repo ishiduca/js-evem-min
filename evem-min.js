f5c419f892dfc5ab9a488949d7f050792bc2a902
ishiduca <ishiduca@gmail.com>
2013-09-21 23:33:17 +0900

    fixed indent

--- evem-min.js
+++ evem-min.js
@@ -1,47 +1,47 @@
 (function (global) {
-	'use strict'
+    'use strict'
 
-	var isBrowser = !! global.self
-	var isWorker  = !! global.WorkerLocation
-	var isNodeJS  = !! global.global
+    var isBrowser = !! global.self
+    var isWorker  = !! global.WorkerLocation
+    var isNodeJS  = !! global.global
 
-	var emitter = {}
-	emitter.constructor = function constructor () {
-		this.evs = {}
-		return this
-	}
-	emitter.on = emitter.addListener = function (name, listener) {
-		;(this.evs[name] || (this.evs[name] = [])).push(listener)
-		return this
-	}
-	emitter.once = function (name, listener) {
-		var me = this
-		function _remove () {
-			listener.apply(null, arguments)
-			me.off(name, _remove)
-		}
-		return this.on(name, _remove)
-	}
-	emitter.emit = function () {
-		var args = [].slice.apply(arguments)
-		var name = args.shift()
-		var evs  = this.evs[name] || []
-		var flg  = false
-		for (var i = 0, l = evs.length; i < l; i++) {
-			typeof evs[i] === 'function' && (flg = true) && evs[i].apply(null, args)
-		}
-		return flg
-	}
-	emitter.off = emitter.removeListener = function (name, listener) {
-		this.evs[name] && (this.evs[name][ this.evs[name].indexOf(listener) ] = null)
-		return this
-	}
+    var emitter = {}
+    emitter.constructor = function constructor () {
+        this.evs = {}
+        return this
+    }
+    emitter.on = emitter.addListener = function (name, listener) {
+        ;(this.evs[name] || (this.evs[name] = [])).push(listener)
+        return this
+    }
+    emitter.once = function (name, listener) {
+        var me = this
+        function _remove () {
+            listener.apply(null, arguments)
+            me.off(name, _remove)
+        }
+        return this.on(name, _remove)
+    }
+    emitter.emit = function () {
+        var args = [].slice.apply(arguments)
+        var name = args.shift()
+        var evs  = this.evs[name] || []
+        var flg  = false
+        for (var i = 0, l = evs.length; i < l; i++) {
+            typeof evs[i] === 'function' && (flg = true) && evs[i].apply(null, args)
+        }
+        return flg
+    }
+    emitter.off = emitter.removeListener = function (name, listener) {
+        this.evs[name] && (this.evs[name][ this.evs[name].indexOf(listener) ] = null)
+        return this
+    }
 
 
-	if (isNodeJS) {
-		module.exports = emitter
-	} else {
-		global.emitter = emitter
-	}
+    if (isNodeJS) {
+        module.exports = emitter
+    } else {
+        global.emitter = emitter
+    }
 
 })(this.self || global)
